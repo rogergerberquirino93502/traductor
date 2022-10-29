cddb8182127edbcd1443266fef8aa1cb3037f1c0
rogergerberquirino93502 <rogergerberquirino@gmail.com>
2022-10-28 22:37:20 -0600

    Corregido y Actualizado

--- config/Script-1.sql
+++ config/Script-1.sql
@@ -1,22 +1,29 @@
 -- traductor.idiomas definition
-CREATE DATABASE traductor;
 
 CREATE TABLE `idiomas` (
   `id_idioma` int(11) NOT NULL AUTO_INCREMENT,
   `nombre_idioma` varchar(255) NOT NULL,
-  `descripcion_idioma` varchar(255) NOT NULL,
-  `date_added` datetime NOT NULL,
   PRIMARY KEY (`id_idioma`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
+
+
+-- traductor.palabras definition
+
+CREATE TABLE `palabras` (
+  `id_palabra` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre_palabra` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_palabra`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
+
 
 -- traductor.traductor definition
 
 CREATE TABLE `traductor` (
   `id_traductor` int(11) NOT NULL AUTO_INCREMENT,
-  `palabra_espaniol` char(20) NOT NULL,
+  `id_palabra` int(11) NOT NULL,
   `palabra_nueva` char(255) NOT NULL,
-  `date_added` datetime NOT NULL,
   `id_idioma` int(11) NOT NULL,
-  `imagen` mediumblob NOT NULL,
   PRIMARY KEY (`id_traductor`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb3;
+
+delete from traductor;
